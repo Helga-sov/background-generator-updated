5f2523ef26e59f8ea1d05c5e49bea20f34d0ca4e
Olga Sandu <sandu.helga@gmail.com>
2022-03-17 14:55:42 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+var _ = require("lodash");
+
+var array = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log('answer:', _.without(array, 3));
+
+
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var randomButton = document.querySelector("button");
+var h3 = document.querySelector(".one");
+
+function setGradient() {
+    body.style.background =
+        "linear-gradient(to right, " +
+        color1.value +
+        ", " +
+        color2.value +
+        ")";
+    css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+setGradient();
+
+randomButton.addEventListener("click", function () {
+    var rgb1 = "rgb(" + (Math.floor(Math.random() * 256)) + ", " + (Math.floor(Math.random() * 256)) + ", " + (Math.floor(Math.random() * 256)) + ")";
+    var rgb2 = "rgb(" + (Math.floor(Math.random() * 256)) + ", " + (Math.floor(Math.random() * 256)) + ", " + (Math.floor(Math.random() * 256)) + ")";
+
+
+
+    body.style.background = "linear-gradient(to right, " + rgb1 + ", " + rgb2 + ")";
+
+    h3.textContent = body.style.background + ";";
+});
